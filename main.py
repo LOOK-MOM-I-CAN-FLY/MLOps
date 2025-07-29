768acf58c6affc88cec4b38ad34044d71f1f886a
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2025-07-29 09:58:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,21 @@ sentiment_pipeline = pipeline(
     model="distilbert-base-uncased-finetuned-sst-2-english",
 )
 
+
 class TextToAnalyze(BaseModel):
     text: str
+
 
 class SentimentResponse(BaseModel):
     label: str
     score: float
 
+
 @app.get("/")
 def read_root():
     """Корневой эндпоинт, который может проверить на доступность API"""
     return {"status": "API is running"}
+
 
 @app.post("/predict", response_model=SentimentResponse)
 def predict_sentiment(payload: TextToAnalyze):
@@ -32,6 +36,7 @@ def predict_sentiment(payload: TextToAnalyze):
     Принимает JSON с полем "text" и возвращает тональность и уверенность.
     """
     result = sentiment_pipeline(payload.text)[0]
-    return SentimentResponse(label=result['label'], score=result['score'])
+    return SentimentResponse(label=result["label"], score=result["score"])
+
 
 # Чтобы запустить локально: uvicorn main:app --reload
